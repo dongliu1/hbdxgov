8c278104f0c68cabeef05fd8704f69aafd5417db
liudong <1137293945@qq.com>
2017-04-15 17:51:01 +0800

    ffff

--- rtdbms/index.js
+++ rtdbms/index.js
@@ -11,7 +11,7 @@ var inc_tabs={
     _init:function () {
         $.each(inc_tabs._tab_menus,function (t, tdata) {
             var _tabid=tdata.tabid;
-            var _src=_tabid+"/index.html";
+            var _src=_tabid+"/index.html?menuid="+tdata.mid;
             var _li="<li tabid='tab-"+_tabid+"' onclick='init_rox_tabs(\""+_tabid+"\",\""+_src+"\")'><a href='javascript:void(0)' "+(tdata.name.length==2?"style='letter-spacing:1em;'":"")+">"+tdata.name+"</a></li>";
             $("#tab-menus").append(_li);
             if(tdata.hasOwnProperty("children"))inc_tabs._init_childtabs(_tabid,tdata.children);
@@ -22,7 +22,7 @@ var inc_tabs={
         var _html="<ul id='tab-"+tabid+"' style='display: none;position: absolute;'>";
         $.each(nodedata,function (n,ndata) {
             var _src=tabid+"/index.html?menuid="+ndata.mid;
-            _html+="<li onclick='init_rox_tabs(\""+tabid+"\",\""+_src+"\")'>"+ndata.name+"</li>"
+            _html+="<li mid='"+ndata.mid+"' onclick='init_rox_tabs(\""+tabid+"\",\""+_src+"\")'>"+ndata.name+"</li>"
         });
         _html+="</ul>";
         $(".content>.inc-tabs").append(_html);
@@ -51,74 +51,74 @@ var inc_tabs={
         })
     },
     _tab_menus:[
-        {name:"首页",tabid:"index"},
-        {name:"校院概况",tabid:"page-profile",children:[
-                {name:"校院简介",mid:""},
-                {name:"现任领导",mid:""},
-                {name:"机构设置",mid:""},
-                {name:"主要职能",mid:""},
-                {name:"历史沿革",mid:""}
+        {name:"首页",mid:"",tabid:"index"},
+        {name:"校院概况",mid:"xyjj",tabid:"page-profile",children:[
+                {name:"校院简介",mid:"xyjj"},
+                {name:"现任领导",mid:"xrld"},
+                {name:"机构设置",mid:"jgsz"},
+                {name:"主要职能",mid:"zyzn"},
+                {name:"历史沿革",mid:"lsyg"}
             ]
         },
-        {name:"教学工作",tabid:"page-course",children:[
-                {name:"教学公告",mid:""},
-                {name:"教学管理",mid:""},
-                {name:"教学动态",mid:""},
-                {name:"教学计划",mid:""},
-                {name:"教学研究",mid:""},
-                {name:"教学资料下载",mid:""}
+        {name:"教学工作",mid:"jxgg",tabid:"page-course",children:[
+                {name:"教学公告",mid:"jxgg"},
+                {name:"教学管理",mid:"jxgl"},
+                {name:"教学动态",mid:"jxdt"},
+                {name:"教学计划",mid:"jxjh"},
+                {name:"教学研究",mid:"jxyj"},
+                {name:"教学资料下载",mid:"jxzlxz"}
             ]
         },
-        {name:"学术科研",tabid:"page-research",children:[
-                {name:"科研动态",mid:""},
-                {name:"管理制度",mid:""},
-                {name:"决策咨询",mid:""},
-                {name:"学术成果",mid:""},
-                {name:"报刊杂志",mid:""},
-                {name:"科研资料下载",mid:""}
+        {name:"学术科研",mid:"kydt",tabid:"page-research",children:[
+                {name:"科研动态",mid:"kydt"},
+                {name:"管理制度",mid:"glzd"},
+                {name:"决策咨询",mid:"jczx"},
+                {name:"学术成果",mid:"xscg"},
+                {name:"报刊杂志",mid:"bkzz"},
+                {name:"科研资料下载",mid:"kyzlxz"}
             ]
         },
-        {name:"学员管理",tabid:"page-student",children:[
-                {name:"学员动态",mid:""},
-                {name:"学员管理",mid:""},
-                {name:"学员天地",mid:""}
+        {name:"学员管理",mid:"xydt",tabid:"page-student",children:[
+                {name:"学员动态",mid:"xydt"},
+                {name:"学员管理",mid:"xygl"},
+                {name:"学员天地",mid:"xytd"}
             ]
         },
-        {name:"队伍建设",tabid:"page-teamwork",children:[
-                {name:"政策法规",mid:""},
-                {name:"工作动态",mid:""},
-                {name:"师资队伍",mid:""},
-                {name:"学科建设",mid:""}
+        {name:"队伍建设",mid:"zcfg",tabid:"page-teamwork",children:[
+                {name:"政策法规",mid:"zcfg"},
+                {name:"工作动态",mid:"gzdt"},
+                {name:"师资队伍",mid:"szdw"},
+                {name:"学科建设",mid:"xkjs"}
             ]
         },
-        {name:"信息化建设",tabid:"page-information",children:[
-                {name:"信息化概况",mid:""},
-                {name:"信息化动态",mid:""},
-                {name:"规章制度",mid:""}
+        {name:"信息化建设",mid:"xxhgk",tabid:"page-information",children:[
+                {name:"信息化概况",mid:"xxhgk"},
+                {name:"信息化动态",mid:"xxhdt"},
+                {name:"规章制度",mid:"gzzd"}
             ]
         },
-        {name:"机关党建",tabid:"page-organ",children:[
-                {name:"工作动态",mid:""},
-                {name:"文件汇编",mid:""},
-                {name:"党风廉政建设",mid:""},
-                {name:"精神文明建设",mid:""},
-                {name:"老干部工作",mid:""}
+        {name:"机关党建",mid:"gzdt",tabid:"page-organ",children:[
+                {name:"工作动态",mid:"gzdt"},
+                {name:"文件汇编",mid:"wjhb"},
+                {name:"党风廉政建设",mid:"dflzjs"},
+                {name:"精神文明建设",mid:"jswmjs"},
+                {name:"老干部工作",mid:"lgbgz"}
             ]
         },
-        {name:"服务保障",tabid:"page-service",children:[
-                {name:"规章制度",pid:"",tabid:"wsd-school"},
-                {name:"工作动态",pid:"",tabid:"wsd-school"},
-                {name:"社会化服务",pid:"",tabid:"wsd-school"},
-                {name:"意见信箱",pid:"",tabid:"wsd-school"}
+        {name:"服务保障",mid:"gzzd",tabid:"page-service",children:[
+                {name:"规章制度",mid:"gzzd"},
+                {name:"工作动态",mid:"gzdt"},
+                {name:"社会化服务",mid:"shhfw"},
+                {name:"意见信箱",mid:"yjxx"}
             ]
         },
-        {name:"智慧校园云平台",tabid:"page-wsdSchool"}
+        {name:"智慧校园云平台",mid:"",tabid:"page-wsdSchool"}
     ]
 };
 
 var inc_content={
     _init:function () {
-        init_rox_tabs("index","index/index.html");
+        init_rox_tabs("index","index/index.html?menuid=");
     }
 };
 
--- rtdbms/rsc/js/rox-function.js
+++ rtdbms/rsc/js/rox-function.js
@@ -13,27 +13,50 @@ function init_rox_tabs(tabid,_src) {
         _iframe.show();
         isExist=true;
     }else{
-        var _html="<iframe id='"+iframeid+"' src='"+_src+"' width='100%'  frameborder='no' border='0' marginwidth='0' marginheight='0' scrolling='no' allowtransparency='yes' style='height:100%;'></iframe>";
+        var _html="<iframe id='"+iframeid+"' name='' src='"+_src+"' onload='init_load_pubtemplate(\""+iframeid+"\")' width='100%'  frameborder='no' border='0' marginwidth='0' marginheight='0' scrolling='no' allowtransparency='yes' style='height: 886px;'></iframe>";
         $(".content>.inc-content").append(_html);
     }
     //判断路径是否传入参数
-    if(param[1]){
-        $(".inc-tabs>ul:not(:first)").hide();
-        if(isExist){
-            init_select_tab(_iframe,param[1]);
-        }else{      //加载完毕选中tab项
-            _iframe.on('load', function() {
-                init_select_tab(_iframe,param[1]);
-            });
-        }
-    }
+    if(param[1])$(".inc-tabs>ul:not(:first)").hide();
+    if(isExist)init_select_tab(_iframe,param[1]);
 }
 
 //选中iframe内对应tab项
 function init_select_tab(_iframe,param) {
-    var menuid=param.split("=")[1];
     var doc=$(_iframe).contents();
+    var menuid=param.split("=")[1];
+    if(!menuid)return;
+    $(".inc-left-menus li",doc).removeClass("tab-active");
     $("[nodeid='tab-"+menuid+"']",doc).addClass("tab-active");
-    var _href=$("[nodeid='tab-"+menuid+"']",doc).attr("href");
-    $("#tab-content",doc).load(_href);
+    _inc_right._init(doc,_iframe);
+}
+
+//加载左部与右部
+function init_load_pubtemplate(_iframeid) {
+    var _iframe=$("#"+_iframeid);
+    var _src=_iframe.attr("src");
+    var param=_src.split("?")[1];
+    console.log(param,_src);
+    if(param.split("=")[1]=="")return;
+    var doc=_iframe.contents();
+    var _html='<div class="contain"><div class="inc-left"></div><div class="inc-right"></div></div>';
+    $("body",doc).html(_html);
+    $(".contain>.inc-left",doc).load("templates/public-page/ifr-left.html",function () {
+        _inc_left._init(doc,_iframeid);
+        var _tabid=_iframeid.replace("iframe","tab");
+        var _html="<ul id='"+_tabid+"-menus'>";
+        $("#"+_tabid).find("li").each(function () {
+            var tabname=$(this).text();
+            var mid=$(this).attr("mid");
+            //console.log(mid);
+            _html+="<li nodeid='tab-"+mid+"' onclick='window.parent._inc_left._onclick(this)'>"+tabname+"</li>"
+        });
+        _html+="</ul>";
+        $(".inc-left-menus",this).html(_html);
+        console.log(_src,param);
+        init_select_tab("#"+_iframeid,param);
+    });
+    $(".contain>.inc-right",doc).load("templates/public-page/ifr-right.html",function () {
+        _inc_right._init(doc,_iframeid);
+    });
 }
